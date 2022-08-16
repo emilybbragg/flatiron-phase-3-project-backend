57ff27fc7d8c4be38ddf10d1c793af0293be6b0e
Emily <emilybribragg@gmail.com>
2022-08-16 15:01:42 -0400

    Added Sinatra routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  get '/exercises' do
+    exercises = Exercise.all
+    exercises.to_json
+  end
+
+  get '/workouts' do
+    workouts = Workouts.all
+    workouts.to_json
   end
 
 end
