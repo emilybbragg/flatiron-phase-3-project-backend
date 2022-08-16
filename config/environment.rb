6fc42d5a7ef498aba2bc86bc172409b41f01448c
Emily <emilybribragg@gmail.com>
2022-08-16 15:03:50 -0400

    Re-added deleted code

--- config/environment.rb
+++ config/environment.rb
@@ -8,3 +8,8 @@ Bundler.require(:default, ENV['RACK_ENV'])
 
 # Require in all files in 'app' directory
 require_all 'app'
+
+ENV["RACK_ENV"] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV["RACK_ENV"])
