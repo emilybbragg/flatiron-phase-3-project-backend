c42048adf292ddab348c532d5b0aa1e013aeac15
Emily <emilybribragg@gmail.com>
2022-08-24 15:38:46 -0400

    Code cleanup

--- db/migrate/20220815174006_create_exercises.rb
+++ db/migrate/20220815174006_create_exercises.rb
@@ -3,7 +3,6 @@ class CreateExercises < ActiveRecord::Migration[6.1]
     create_table :exercises do |t|
       t.string :name
       t.string :description
-      t.string :category
     end
   end
 end
