55f60957536bff69a1ff8e40aaccf59394b391fb
Emily <emilybribragg@gmail.com>
2022-08-24 15:36:33 -0400

    Edited patch request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,70 @@ class ApplicationController < Sinatra::Base
     exercises.to_json
   end
 
+
   get '/workouts' do
-    workouts = Workouts.all
+    workouts = Workout.all
     workouts.to_json
   end
 
-end
+
+  get '/categories' do
+    categories = Category.all
+    categories.to_json
+  end
+
+  get '/exercise-workouts' do
+    exerciseWorkouts = ExerciseWorkout.all
+    exerciseWorkouts.to_json
+  end
+
+
+  delete '/exercises/:id' do
+    exercise = Exercise.find(params[:id])
+    exercise.destroy
+    exercise.to_json
+  end
+
+
+  delete '/workouts/:id' do
+    workout = Workout.find(params[:id])
+    workout.destroy
+    workout.to_json
+  end
+
+
+  post '/exercises' do
+    exercise = Exercise.create(
+      name: params[:name],
+      description: params[:description],
+      category_id: params[:category_id],
+    )
+    exercise.to_json
+  end
+
+
+  post '/workouts' do
+    workout = Workout.create(
+      name: params[:name],
+      description: params[:description],
+    )
+    workout.to_json
+  end
+
+
+  patch '/workouts/:id' do
+    exercise_workouts = []
+
+    params[:exercises].each do |exercise_id|
+    
+      exercise_workout = ExerciseWorkout.create(
+        exercise_id: exercise_id,
+        workout_id: params[:id],
+      )
+
+      exercise = Exercise.find(exercise_id)
+      exercise_workouts.push(exercise)
+      end
+    exercise_workouts.to_json
+    end
+  end
