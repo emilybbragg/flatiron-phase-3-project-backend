971e15c7727b2d5cba20180c482dc11992ad2ff0
Emily <emilybribragg@gmail.com>
2022-09-06 20:28:53 -0400

    Final code cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,46 +3,38 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  # GET request for retrieving all exercises
   get '/exercises' do
     exercises = Exercise.all
     exercises.to_json
   end
 
-  # GET request for retrieving all workouts
   get '/workouts' do
     workouts = Workout.all
     workouts.to_json
   end
 
-  # GET request for retrieving all categories
   get '/categories' do
     categories = Category.all
     categories.to_json
   end
 
-  # GET request for retrieving all exercises for each workout
   get '/exercise-workouts' do
     exerciseWorkouts = ExerciseWorkout.all
     exerciseWorkouts.to_json
   end
 
-  # DELETE request for deleting an exercise
   delete '/exercises/:id' do
     exercise = Exercise.find(params[:id])
     exercise.destroy
     exercise.to_json
   end
 
-  # DELETE request for deleting a workout
   delete '/workouts/:id' do
     workout = Workout.find(params[:id])
     workout.destroy
     workout.to_json
   end
 
-  # POST request for creating an exercise
-  # each exercise has a name, description and a category id
   post '/exercises' do
     exercise = Exercise.create(
       name: params[:name],
@@ -52,7 +44,6 @@ class ApplicationController < Sinatra::Base
     exercise.to_json
   end
 
-  # POST request for creating a workout
   post '/workouts' do
     workout = Workout.create(
       name: params[:name],
@@ -61,13 +52,6 @@ class ApplicationController < Sinatra::Base
     workout.to_json
   end
 
-  # PATCH request for updating a workout
-  # create new array consisting of all exercises for each workout
-  # for each exercise, take in arg of exercise id
-  # create variable for each new exercise for workout, consisting of an exercise id and workout id
-  # find exercise based on id
-  # add matching exercise to array of all exercises
-  # display all exercises for each workout
   patch '/workouts/:id' do
 
     ExerciseWorkout.where(workout_id: params[:id]).destroy_all
@@ -86,4 +70,5 @@ class ApplicationController < Sinatra::Base
       end
     exercise_workouts.to_json
     end
+    
   end
